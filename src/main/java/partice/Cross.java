b9e67836d61ea12ad503728e14c445c76fe035cc
babu <karanamseshubabu28@gmail.com>
2024-09-08 12:18:15 +0530

    updated

--- src/main/java/partice/Cross.java
+++ src/main/java/partice/Cross.java
@@ -1,12 +1,10 @@
 package partice;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
-import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,25 +12,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Cross {
 
 	public static void main(String[] args) throws IOException {
+		/*
+		 * ChromeOptions chromeOptions = new ChromeOptions();
+		 * chromeOptions.addArguments("incognito")
+		 */;
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
-		List<WebElement> tags = driver.findElements(By.tagName("a"));
-		int count = 0;
-		for (WebElement tag : tags) {
-			String str = tag.getAttribute("href");
-			URL url = new URL(str);
-			HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-			connect.connect();
-			if (connect.getResponseCode() >= 200) {
-				count++;
-				System.out.println(str);
-			}
+		driver.get("https://www.google.co.in/");
+		WebElement elemnt = driver.findElement(By.xpath("//textarea[@class='gLFyf']"));
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("arguments[0].value='seshu'", elemnt);
+		elemnt.click();
+		List<WebElement> elements = driver.findElements(By.xpath("//ul//li//div[@class=\"pcTkSc\"]"));
+		for (WebElement element : elements) {
+			System.out.println(element.getText());
 		}
-		System.out.println(count);
-		Set<String> winId = driver.getWindowHandles();
-		for(String win: winId) {
-			driver.switchTo().window(win);
-		}
-
+		
 	}
 }
